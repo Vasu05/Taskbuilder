ced68bd9626f9ac337c12dabb2264c9f9c642ed9
Vasu <Vasu14114@iiitd.ac.in>
2016-11-09 20:20:23 +0530

    Update viewpager.java

--- app/src/main/java/com/example/vasuchand/taskbuilder/viewpager.java
+++ app/src/main/java/com/example/vasuchand/taskbuilder/viewpager.java
@@ -4,20 +4,52 @@ import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class viewpager extends AppCompatActivity {
      ViewPager vp ;
      Intent i;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewpager);
         vp = (ViewPager)findViewById(R.id.viewpager);
+        toolbar =(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        }else{
+            System.out.println("sfsd");
+        }
         vp.setAdapter(new viewpager_Adapter(this,MainActivity.movieList));
         i = getIntent();
         int a = i.getIntExtra("position",0);
         System.out.println(a);
         vp.setCurrentItem(a);
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                Intent intent = new Intent(viewpager.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(viewpager.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        startActivity(intent);
     }
 }
