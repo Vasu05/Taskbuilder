162a3a97a95451af09089c95ce28afe0f0274ca3
Vasu <Vasu14114@iiitd.ac.in>
2016-11-09 20:20:56 +0530

    Update add_task.java

--- app/src/main/java/com/example/vasuchand/taskbuilder/add_task.java
+++ app/src/main/java/com/example/vasuchand/taskbuilder/add_task.java
@@ -3,6 +3,8 @@ package com.example.vasuchand.taskbuilder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +16,7 @@ public class add_task extends AppCompatActivity {
     private EditText e1,e2;
     private Button b1;
     data dta;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +24,21 @@ public class add_task extends AppCompatActivity {
         e1 = (EditText)findViewById(R.id.e1);
         e2 = (EditText)findViewById(R.id.e2);
         b1 = (Button)findViewById(R.id.b3);
+        toolbar =(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        }else{
+            System.out.println("sfsd");
+        }
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String a = e1.getText().toString();
                 String b = e2.getText().toString();
                 System.out.println(a + " "+  b);
-                if(a !=null || !a.isEmpty()&& b!=null ||!b.isEmpty() )
+                if(a !=null || !a.isEmpty() && b!=null ||!b.isEmpty() )
                 {
                     dta = new data(a,b);
                     MainActivity.movieList.add(dta);
@@ -42,5 +53,26 @@ public class add_task extends AppCompatActivity {
                 }
             }
         });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                Intent intent = new Intent(add_task.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(add_task.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        startActivity(intent);
     }
 }
